bd0c7521e03ab662f7b9861613c9485c892e48c8
Victor Yan <vicyan614@gmail.com>
2018-11-23 23:24:00 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@
 	echo stopped old container &&
 	sleep 2
 	echo starting new container
-	docker run --hostname=victor-computer --name=ssh_server -d -p 22:22 --rm ssh_container
+	docker run --hostname=ssh-server --name=ssh_server -d -p 22:22 --rm ssh_container
 } || {
 	echo starting new container
-	docker run --hostname=victor-computer --name=ssh_server -d -p 22:22 --rm ssh_container
+	docker run --hostname=ssh-server --name=ssh_server -d -p 22:22 --rm ssh_container
 }
